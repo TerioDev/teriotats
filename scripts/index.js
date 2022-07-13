31281c68a4f3cea45d6422d6d33e9b47608b0226
TerraMsc <103744671+TerraMsc@users.noreply.github.com>
2022-07-13 02:22:10 +0200

    booking window redesign

--- scripts/index.js
+++ scripts/index.js
@@ -17,11 +17,13 @@ view1.fromTo(".navigation", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0
 $(".book-button").on("click",function(){
     var book = gsap.timeline({});
     
+    book.to(".book-overlay", {opacity: 1, duration: 0}, 0)
+    book.to(".book-wrapper", {opacity: 1, duration: 0}, 0)
     book.to(".book-wrapper", {zIndex: 2, duration: 0}, 0)
-    book.to(".book-overlay", {opacity: 1, duration: 0.5}, 0)
-    book.to(".book-wrapper", {opacity: 1, duration: 0.5}, 0)
-    book.fromTo(".book-window", {scaleX:0.4, scaleY:0.4}, {scaleX:1, scaleY:1, duration: 0.5, ease: "elastic.out(0.8, 1.4)"}, 0)
-    book.fromTo(".x", {opacity: 0}, {opacity:1, duration: 0.6, ease: "power2.out"}, 0.5)
+    book.to(".shadow", {zIndex: 1, duration: 0}, 0)
+    book.fromTo(".book-window", {y: "-100vh"}, {y:0, duration: 1.5, ease: "power3.out"}, 0)
+    book.to(".shadow", {opacity: 1, duration: 1.2, ease: "power3.out"}, 0.3)
+    book.fromTo(".x", {opacity: 0}, {opacity:1, duration: 0.6, ease: "power3.out"}, 1.2)
 });
 
 // CLOSE BOOK WINDOW
@@ -29,43 +31,41 @@ $(".book-button").on("click",function(){
 $(".x").on("click",function(){
     var x = gsap.timeline({});
 
-    x.to(".book-wrapper", {opacity: 0, duration: 0.3}, 0)
-    x.fromTo(".book-window", {scaleX:1}, {scaleX:0.95, scaleY:0.95, duration: 0.3}, 0)
-    x.to(".book-overlay", {opacity: 0, duration: 0.3}, 0.3)
-    x.to(".book-window", {borderRadius: "60px"}, 0)
-    x.to(".info", {borderTopLeftRadius: "55px", borderBottomLeftRadius: "55px", duration: 0.3}, 0)
-    x.to(".form", {borderBottomRightRadius: "55px", borderTopRightRadius: "55px", duration: 0.3}, 0)
-    x.to(".book-wrapper", {zIndex: 0, duration: 0}, 0.3)
+    x.to(".book-overlay", {opacity: 0, duration: 0}, 0)
+    x.fromTo(".book-window", {y:0}, {y: "-100vh", duration: 0.6, ease: "power3.out"}, 0)
+    x.to(".shadow", {opacity: 0, duration: 0.5, ease: "power3.out"}, 0.1)
+    x.to(".book-wrapper", {zIndex: 0, duration: 0}, 0.6)
+    x.to(".shadow", {zIndex: 0, duration: 0}, 0.6)
 });
 
-$(".x").on("click",function(){
+/* $(".x").on("click",function(){
     $(".info").animate({
         scrollTop: 0
     }, 1000, 'swing');
-});
+}); */
 
 // EXPAND TIMELINE
-var expand = gsap.timeline({});
+/* var expand = gsap.timeline({});
 
-expand.to(".book-window", {border: 0, borderRadius: 0, duration: 0.5, width: "100vw", height: "100vh", margin: 0, padding: 0, bottom: 0}, 0)
+expand.to(".book-window", {}, 0)
 expand.to(".info", {borderRadius: 0}, 0)
-expand.to(".form", {borderRadius: 0}, 0)
+expand.to(".form", {borderRadius: 0}, 0) */
 
 // EXPAND BOOK WINDOW
 // media query
-const outputElement = document.getElementById("info");
+/* const outputElement = document.getElementById("info"); */
 
 // for less than x px
-const smallDevice = window.matchMedia("(min-width: 800px)");
+/* const smallDevice = window.matchMedia("(min-width: 800px)");
 
-smallDevice.addListener(handleDeviceChange);
+smallDevice.addListener(handleDeviceChange); */
 
 // state variables
-let left = true;
-let right = true;
+/* let left = true;
+let right = true; */
 
 // expand functions
-function handleDeviceChange(e) {
+/* function handleDeviceChange(e) {
   if (e.matches) {
     ScrollTrigger.create({
         scroller: '.info',
@@ -104,6 +104,7 @@ function checkLeft() {
 
     if (left == true && right == true) {
         console.log("playing");
+        
         expand.reverse();
     }
 }
@@ -113,9 +114,10 @@ function checkRight() {
 
     if (left == true && right == true) {
         console.log("playing");
+        
         expand.reverse();
     }
-}
+} */
 
 // Run it initially
-handleDeviceChange(smallDevice);
+/* handleDeviceChange(smallDevice); */
