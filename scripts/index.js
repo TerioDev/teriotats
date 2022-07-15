c8f29bb83d6ab0657b546caad6f09a8e2de2db11
TerraMsc <103744671+TerraMsc@users.noreply.github.com>
2022-07-15 23:18:31 +0200

    back fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,13 @@ gsap.registerPlugin(ScrollTrigger);
 
 // RELOAD
 
-var book = gsap.timeline({});
+/* var book = gsap.timeline({});
     
 book.to(".book-overlay", {opacity: 0, duration: 0}, 0)
 book.to(".shadow", {zIndex: -1, duration: 0}, 0)
-book.to(".shadow", {opacity: 0, duration: 0}, 0)
+book.to(".shadow", {opacity: 0, duration: 0}, 0) */
+
+$('.grid').animate({ opacity: 0 }, 0).css("background-image", "url(design/sq.png)").delay(800).animate({ opacity: 0.8 }, 1600);
 
 var view1 = gsap.timeline({});
 
@@ -19,5 +21,3 @@ view1.fromTo(".divider", {width: 0}, {width: "100%", duration: 1.5, ease: "power
 view1.fromTo("#navigation", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
 
 view1.delay(0.2)
-
-$('.grid').animate({ opacity: 0 }, 0).css("background-image", "url(design/sq.png)").delay(800).animate({ opacity: 0.8 }, 1600);
