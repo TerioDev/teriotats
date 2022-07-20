35d77164526ee39c786d59b7212e60e99b2867e6
TerraMsc <103744671+TerraMsc@users.noreply.github.com>
2022-07-20 16:08:32 +0200

    v0.4: reload fix and view1 animation adjust

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,27 @@
 gsap.registerPlugin(ScrollTrigger);
 
+var view1 = gsap.timeline({});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    view1.fromTo('.typ1', {opacity: 0}, {opacity: 1, duration: 0.4})
+    view1.to(".typ2", {opacity: 1, duration: 1.9, ease: "power2.in"}, 0.4)
+    view1.to(".typ2r", {opacity: 1, duration: 1.9,  ease: "power2.in"}, 0.4)
+    view1.to(".typ1", {opacity: 0, duration: 1}, 1.9)
+    
+    view1.fromTo(".divider", {width: 0}, {width: "100%", duration: 1.5, ease: "power3.out"}, 0.8)
+    
+    view1.fromTo("#navigation", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
+    view1.fromTo("#mobile-navigation-wrap", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
+
+}, false);
+
+
+var book = gsap.timeline({});
+
 $(".book-button").on("click",function(){
 
-    var book = gsap.timeline({});
-    
     book.to(".book-overlay", {opacity: 1, duration: 0.2}, 0)
     book.to(".book-overlay", {opacity: 0, duration: 0.2}, 0.6)
 
 });
-
-/* $('.grid').animate({ opacity: 0 }, 0).css("background-image", "url(design/sq.png)").delay(800).animate({ opacity: 0.8 }, 1600); */
-
-var view1 = gsap.timeline({});
-
-view1.delay(0.1)
-
-view1.to(".typ2", {opacity: 1, duration: 2, ease: "power1.out"}, 0)
-view1.to(".typ2r", {opacity: 1, duration: 2,  ease: "power1.out"}, 0)
-view1.to(".typ1", {opacity: 0, duration: 1}, 1)
-
-view1.fromTo(".divider", {width: 0}, {width: "100%", duration: 1.5, ease: "power3.out"}, 0.8)
-
-view1.fromTo("#navigation", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
-view1.fromTo("#mobile-navigation-wrap", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
