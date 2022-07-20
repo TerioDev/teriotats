4e0167061d29ed01254516c741e73560b90f9f5e
TerraMsc <103744671+TerraMsc@users.noreply.github.com>
2022-07-20 17:40:02 +0200

    v0.4.1: fixed moblie refresh

--- scripts/book-button-index.js
+++ scripts/book-button-index.js
@@ -1,8 +1,13 @@
+var book = gsap.timeline({});
+
 $('.book-button').click (function (e) {
         e.preventDefault(); //will stop the link href to call the blog page
 
-        gsap.to(".wrapper", {opacity: 0, duration: 0.7})
-        navigationOnClickMobile.reverse(0.7)
+        book.to(".book-overlay", {opacity: 1, duration: 0.2}, 0)
+        book.to(".book-overlay", {opacity: 0, duration: 0.2}, 0.6)
+
+        view1.timeScale(4).reverse()
+        navigationOnClickMobile.timeScale(3).reverse()
      
         setTimeout(function () {
             window.location.href = "book.html"; //will redirect to your blog page (an ex: blog.html)
--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,14 @@ gsap.registerPlugin(ScrollTrigger);
 
 var view1 = gsap.timeline({});
 
-document.addEventListener('DOMContentLoaded', function() {
-
     view1.fromTo('.typ1', {opacity: 0}, {opacity: 1, duration: 0.4})
     view1.to(".typ2", {opacity: 1, duration: 1.9, ease: "power2.in"}, 0.4)
     view1.to(".typ2r", {opacity: 1, duration: 1.9,  ease: "power2.in"}, 0.4)
-    view1.to(".typ1", {opacity: 0, duration: 1}, 1.9)
+    view1.to(".typ1", {opacity: 0, duration: 0.1}, 2.3)
     
     view1.fromTo(".divider", {width: 0}, {width: "100%", duration: 1.5, ease: "power3.out"}, 0.8)
     
-    view1.fromTo("#navigation", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
-    view1.fromTo("#mobile-navigation-wrap", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
+var view1Navigation = gsap.timeline({});
 
-}, false);
-
-
-var book = gsap.timeline({});
-
-$(".book-button").on("click",function(){
-
-    book.to(".book-overlay", {opacity: 1, duration: 0.2}, 0)
-    book.to(".book-overlay", {opacity: 0, duration: 0.2}, 0.6)
-
-});
+view1Navigation.fromTo("#navigation", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
+view1Navigation.fromTo("#mobile-navigation-wrap", {y: -160}, {y:0, duration: 1.5, ease: "elastic.out(0.8, 0.5)"}, 0.8)
