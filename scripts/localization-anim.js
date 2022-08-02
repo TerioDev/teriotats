c17f89d317ed98b33ed8adcb42d0e2bb57f0ca3c
TerraMsc <103744671+TerraMsc@users.noreply.github.com>
2022-08-02 18:21:26 +0200

    v1.0.0: release, responsiveness fixes, form fix

--- scripts/localization-anim.js
+++ scripts/localization-anim.js
@@ -1,6 +1,6 @@
 var localClick = gsap.timeline({paused: true})
 
-localClick.to('#public', {x: 190, duration: 0.5, ease: "Power4.easeInOut"})
+localClick.to('.public-wrap', {width: "100%", duration: 0.5, ease: "Power4.easeInOut"})
 localClick.to('.switchlang', {x: -80, duration: 0.5, ease: "Power4.easeInOut"}, "<")
 localClick.fromTo('.switch-wrap', {backgroundColor: "#e3806d"}, {backgroundColor: "#8aa218", duration: 0.5, ease: "Power4.easeInOut"}, "<")
 
